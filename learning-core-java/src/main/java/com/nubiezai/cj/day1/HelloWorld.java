168a46ee7a4b26d92445e435863ff3527eda7095
nubiezai <nubiezai@gmail.com>
2026-08-28 07:54:37 +0530

    Initial commit

---
+++ learning-core-java/src/main/java/com/nubiezai/cj/day1/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.nubiezai.cj.day1;
+
+public class HelloWorld {
+
+    static void main() {
+        IO.println("Welcome to java world");
+    }
+}
--- learning-corejava/src/main/java/com/nubiezai/lcs/day1/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.nubiezai.lcs.day1;
-
-public class HelloWorld {
-
-    static void main(String[] args) {
-        System.out.println("Welcome to java world");
-    }
-}
