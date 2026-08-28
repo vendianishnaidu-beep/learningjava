69fdd351470684233f21a62c24a7b7e77d0c137a
nubiezai <nubiezai@gmail.com>
2026-08-28 08:21:37 +0530

    added new program to find sum of two numbers.

---
+++ learning-core-java/src/main/java/com/nubiezai/cj/day1/SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+package com.nubiezai.cj.day1;
+
+public class SumOfTwoNumbers {
+
+    static void main() {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        IO.println(num1 + " + " + num2 + " = " + sum);
+    }
+}
